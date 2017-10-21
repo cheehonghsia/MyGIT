f6b48d6dec2f23ca57803de0a1989922d69b2389
Cheehong Hsia <hsia.cheehong@gmail.com>
2017-10-21 22:58:30 +0200

    Added Dao class

---
+++ src/main/java/com/chh/xia/dao/PersonDAO.java
@@ -0,0 +1,12 @@
+package com.chh.xia.dao;
+
+import com.chh.xia.domain.Person;
+
+/**
+ * Data Access Object of a Person.
+ */
+public interface PersonDAO {
+
+	public void add(Person person);
+
+}
---
+++ src/main/java/com/chh/xia/service/PersonServiceImpl.java
@@ -0,0 +1,12 @@
+package com.chh.xia.service;
+
+import com.chh.xia.domain.Person;
+
+public class PersonServiceImpl implements PersonService {
+
+	@Override
+	public void addPerson(Person person) {
+
+	}
+
+}
