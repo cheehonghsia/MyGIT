f790a1e2795c16aab46b8f1e9c9597407fcc4b19
Cheehong Hsia <hsia.cheehong@gmail.com>
2017-10-29 08:35:03 +0100

    Refactored class to make it more maintenanceable

--- src/test/java/com/chh/xia/controller/RootControllerTest.java
+++ src/test/java/com/chh/xia/controller/RootControllerTest.java
@@ -4,18 +4,26 @@ import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.chh.xia.calculateservice.CalculateService;
 
 public class RootControllerTest {
+	
+	private static RootController root;
+	private static CalculateService calculateService;
+
+	@BeforeClass
+	public static void initiateCalculateService() {
+		root = new RootController();
+		calculateService = mock(CalculateService.class);
+		when(calculateService.addTwoNumbers(1, 2)).thenReturn(3);
+	}
 
 	@Test
 	public void testAddingFunctionality() {
-		RootController controller = new RootController();
-		int result = controller.someLogic(1, 2);
-		CalculateService calculateService = mock(CalculateService.class);
-		when(calculateService.addTwoNumbers(1, 2)).thenReturn(3);
+		int result = root.someLogic(1, 2);
 		assertTrue(result == 30);
 	}
 }
